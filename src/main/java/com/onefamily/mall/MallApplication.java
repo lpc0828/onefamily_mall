02c111567eda21c05412d1a78c6b7c1e5cacc991
pcliu <liupengcheng@daling.com>
2017-11-22 08:09:43 +0800

    调整代码

--- src/main/java/com/onefamily/mall/MallApplication.java
+++ src/main/java/com/onefamily/mall/MallApplication.java
@@ -2,9 +2,18 @@ package com.onefamily.mall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class MallApplication {
+
+	@RequestMapping("/")
+	@ResponseBody String index() {
+		return "hello world!!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MallApplication.class, args);
